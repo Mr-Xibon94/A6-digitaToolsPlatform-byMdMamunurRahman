62481a227cf563724b6c126c1e4d0a6627da2c9e
Mr-Xibon94 <md19jibon94@gmail.com>
2026-03-30 16:07:11 +0600

    added buttol toggling function

--- src/Components/Allcards.jsx
+++ src/Components/Allcards.jsx
@@ -1,16 +1,21 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 import Cards from './Cards';
+import SelectedCart from './SelectedCart';
 
 const Allcards = ({ cardsDataPromise }) => {
     const allCardsData = use(cardsDataPromise);
 
-    console.log(allCardsData);
+    // it is state for toggling buttons 
+    const [togleBtn, settogleBtn] = useState('products')
+
+    
+    
     return (
         <div>
-            <h1>I am All Cards section</h1>
+            
 
             {/* it is my full card section  */}
-            <div className='border border-red-600 w-11/12 mx-auto'>
+            <div className=' w-11/12 mx-auto'>
                 {/* it is title head div of the card section  */}
                 <div className='text-center'>
                     <h1 className='text-4xl font-bold '>Premium Digital Tools</h1>
@@ -18,21 +23,37 @@ const Allcards = ({ cardsDataPromise }) => {
                     <div className='flex justify-center '>
 
                         {/* it is the two buttons which will togle  */}
-                        <button className='btn rounded-3xl text-white bodyColor'>Products</button>
-                        <button className='btn rounded-3xl'>Cart(0)</button>
+                        <button onClick={()=>(
+                            settogleBtn('products')
+                        )} className={`btn rounded-3xl ${togleBtn=='products'? 'bodyColor text-white':''}`}>Products</button>
+                        {/* BUTTON CART  */}
+                        <button onClick={()=>(
+                            settogleBtn('cart')
+                        )}  className={`btn rounded-3xl ${togleBtn=='cart'? 'bodyColor text-white':''}`}>Cart(0)</button>
                     </div>
                 </div>
 
                 {/* it is the all card interface  */}
-                <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-2.5 mt-10'>
-                    {
+                <div >
+                    { togleBtn === "products"? 
+                    <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-2.5 mt-10'>
 
-                        allCardsData.map(card => (
-                            <Cards card={card}>
+                        {
+    
+                            allCardsData.map(card => (
+                                <Cards key={card.id} card={card}>
+    
+                                </Cards>
+                            ))
+                        } 
+                    </div> : togleBtn === 'cart'?
+                    <div className='border border-red-500 text-center flex justify-center items-center h-50 mt-10'>
+                        <SelectedCart>
 
-                            </Cards>
-                        ))
-                    }
+                        </SelectedCart>
+                    </div> : ''
+                    
+                }
 
                 </div>
 
---
+++ src/Components/SelectedCart.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SelectedCart = () => {
+    return (
+        <div>
+            <h1>here is all selected cart</h1>
+        </div>
+    );
+};
+
+export default SelectedCart;
